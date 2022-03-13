5267911376723ef992c916a1933c08ca76d3e698
Luana Kiefer <luana_kiefer@hotmail.com>
2022-03-12 21:08:39 -0300

    Transformar arquivo baixado em JSON

--- src/hycom/download.py
+++ src/hycom/download.py
@@ -1,5 +1,7 @@
 import requests
+from netCDF4 import Dataset
 from sys import argv
+from json import dump
 
 class Secao():
     """
@@ -39,33 +41,53 @@ def formatar_mes(mes: int) -> str:
     return ''
 
 
-def construir_url(secao: Secao, dia: int, mes: int, ano: int) -> str:
+def construir_url(secao: Secao, dia_1: int, mes_1: int, ano_1: int, dia_2: int, mes_2: int, ano_2: int) -> str:
     """
     Constroi a URL de busca para o hycom
     """
 
-    return f'http://ncss.hycom.org/thredds/ncss/GLBu0.08/expt_19.1/{ano}?var=salinity&var=water_temp&north={secao.norte}&west={secao.oeste}&east={secao.leste}&south={secao.sul}&disableLLSubset=on&disableProjSubset=on&horizStride=1&time_start={ano}-{formatar_mes(mes)}-{formatar_dia(dia)}T00%3A00%3A00Z&time_end={ano}-{formatar_mes(mes)}-{formatar_dia(dia)}T00%3A00%3A00Z&timeStride=1&vertCoord=&accept=netcdf'
+    return f'http://ncss.hycom.org/thredds/ncss/GLBu0.08/expt_19.1/{ano_1}?var=salinity&var=water_temp&north={secao.norte}&west={secao.oeste}&east={secao.leste}&south={secao.sul}&disableLLSubset=on&disableProjSubset=on&horizStride=1&time_start={ano_1}-{formatar_mes(mes_1)}-{formatar_dia(dia_1)}T00%3A00%3A00Z&time_end={ano_2}-{formatar_mes(mes_2)}-{formatar_dia(dia_2)}T00%3A00%3A00Z&timeStride=1&vertCoord=&accept=netcdf'
 
-def construir_arquivo(base: str, dia: int, mes: int, ano: int) -> str:
+def construir_arquivo_nc(base: str, dia_1: int, mes_1: int, ano_1: int, dia_2: int, mes_2: int, ano_2: int) -> str:
     """
     Constroi o nome do arquivo .nc
     """
 
-    return f'{base}_{ano}{formatar_mes(mes)}{formatar_dia(dia)}.nc'
+    return f'{base}_{ano_1}{formatar_mes(mes_1)}{formatar_dia(dia_1)}_{ano_2}{formatar_mes(mes_2)}{formatar_dia(dia_2)}.nc'
+
+def construir_arquivo_json(base: str, dia_1: int, mes_1: int, ano_1: int, dia_2: int, mes_2: int, ano_2: int) -> str:
+    """
+    Constroi o nome do arquivo .json
+    """
+
+    return f'{base}_{ano_1}{formatar_mes(mes_1)}{formatar_dia(dia_1)}_{ano_2}{formatar_mes(mes_2)}{formatar_dia(dia_2)}.json'
+
 
 if __name__ == '__main__':
     dados: list = argv[1:]
 
     secao: Secao = Secao(norte=float(dados[0]), sul=float(dados[1]), leste=float(dados[2]), oeste=float(dados[3]))
-    dia: int = int(dados[4])
-    mes: int = int(dados[5])
-    ano: int = int(dados[6])
+    [dia_1, mes_1, ano_1] = [int(valor) for valor in dados[4].split('-')]
+    [dia_2, mes_2, ano_2] = [int(valor) for valor in dados[5].split('-')]
 
-    url: str = construir_url(secao, dia, mes, ano)
-    arquivo: str = construir_arquivo(dados[7], dia, mes, ano)
+    url: str = construir_url(secao, dia_1, mes_1, ano_1, dia_2, mes_2, ano_2)
+    arquivo_nc: str = construir_arquivo_nc(dados[6], dia_1, mes_1, ano_1, dia_2, mes_2, ano_2)
+    arquivo_json: str = construir_arquivo_json(dados[6], dia_1, mes_1, ano_1, dia_2, mes_2, ano_2)
 
     resposta = requests.get(url)
 
-    with open(arquivo, 'wb') as fd:
+    with open(arquivo_nc, 'wb') as fd:
         for pacote in resposta.iter_content(100):
             fd.write(pacote)
+    
+    dataset = Dataset(arquivo_nc)
+    
+    with open(arquivo_json, 'w') as fd:
+        conteudo = {
+            'LATITUDE': dataset['lat'][:].tolist(),
+            'LONGITUDE': dataset['lon'][:].tolist(),
+            'SALINIDADE': dataset['salinity'][:].tolist(),
+            'TEMPERATURA': dataset['water_temp'][:].tolist()
+        }
+
+        dump(conteudo, fd)
